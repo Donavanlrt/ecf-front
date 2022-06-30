f45e90d2142a4ce6006dd8ee85aeb93194ae4793
Matthieu Mota <matthieumota@gmail.com>
2022-06-30 16:55:00 +0200

    JS part 1

--- html/script.js
+++ html/script.js
@@ -1,2 +1,35 @@
 let $ = (el) => document.querySelector(el);
 let $$ = (el) => document.querySelectorAll(el);
+
+$('.closed').addEventListener('click', function () {
+    this.style.display = 'none';
+    $('.open').style.display = 'block';
+    $('.mobile-menu').style.display = 'block';
+});
+
+$('.open').addEventListener('click', function () {
+    this.style.display = 'none';
+    $('.closed').style.display = 'block';
+    $('.mobile-menu').style.display = 'none';
+});
+
+function orderNow() {
+    $$('#section-1 .w-1\\/2').forEach(el => el.style.display = 'none');
+    $('#title').style.display = 'block';
+
+    setTimeout(() => {
+        $$('#section-1 .w-1\\/2').forEach(el => el.style.display = 'block');
+        $('#title').style.display = 'none';
+    }, 5000);
+}
+
+function playVideo() {
+    $('#video img').style.display = 'none';
+    $('#video iframe').style.display = 'block';
+}
+
+function addYear(event, year) {
+    event.preventDefault();
+
+    $('#year').innerHTML = parseInt($('#year').innerHTML) + year;
+}
