b3ab80ee8a23f6451c27c0568ad9b56df146f554
Matthieu Mota <matthieumota@gmail.com>
2022-06-30 21:26:02 +0200

    Display meals

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Meal from './Meal'
+import meals from './meals'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { meals }
+  }
+
   render() {
     return (
-      <div>Vite + React</div>
+      <div className="px-8 py-32 max-w-7xl mx-auto">
+        <div className="flex justify-between items-center flex-col xl:flex-row">
+          <h1 className="text-3xl sm:text-5xl font-extrabold text-[#243e63]">
+            Découvrez notre <span className="bg-[#6415ff] text-white -skew-x-12 inline-block p-3">menu.</span>
+          </h1>
+        </div>
+
+        <div className="flex flex-wrap justify-between -mx-6 mt-6">
+          {meals.map((meal, key) => <Meal meal={meal} key={key} />)}
+        </div>
+      </div>
     )
   }
 }
---
+++ react/src/Meal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class Meal extends React.Component {
+  render() {
+    return (
+      <div className="w-full sm:w-1/2 md:w-1/3 xl:w-1/4 px-6 mt-10">
+        <div className="relative">
+          <img className="h-64 w-full object-cover rounded-t" src={this.props.meal.imageSrc} alt={this.props.meal.title} />
+
+          <div className="flex items-end absolute bottom-3 left-3 bg-gray-100 rounded-2xl px-5 py-2">
+            <div className="inline-flex items-end">
+              <svg className="w-4 h-4 text-yellow-500 mr-1" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5T1385 1619q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5T365 1569q0-6 2-20l86-500L89 695q-25-27-25-48 0-37 56-46l502-73L847 73q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path></svg>
+              <span className="text-sm mr-1 font-bold text-gray-800 leading-none">{this.props.meal.rating}</span>
+            </div>
+            <span className="text-xs text-gray-500 font-medium leading-none"> ({this.props.meal.reviews})</span>
+          </div>
+        </div>
+        <div className="p-4 bg-slate-100 rounded-b">
+          <h2 className="text-lg font-semibold">{this.props.meal.title}</h2>
+          <p className="text-sm font-medium text-slate-500 mt-1">{this.props.meal.content}</p>
+          <p className="mt-4 font-bold text-xl">{this.props.meal.price}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Meal
